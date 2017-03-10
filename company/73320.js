eced70fd3cc064bd17bc6bf2d0b5a4c45f30463e
hjjkkk <979739697@qq.com>
2017-03-10 23:58:58 +0800

    company

---
+++ company/73320.js
@@ -0,0 +1,22 @@
+(function(){
+    var _qha_data = {};
+    _qha_data['domain']=73320;
+    _qha_data['e360']='2794420307';
+    _qha_data['pageClk']=null;
+    _qha_data['urlClk']=0;
+    _qha_data['idClk']=null;
+    _qha_data['mvid']='287497';
+    _qha_data['host']='s.union.360.cn';
+    window._qha_data = _qha_data;
+    var element=document.createElement('script');
+    element.type='text/javascript';
+    element.async=true;
+    var src="http://s3.qhres.com/static/9eb94fd4eae2a1d6.js";
+    if(document.location.protocol === 'https:'){
+        src = src.replace(/http:\/\/([s|p])[0-9].(qhimg|qhres).com/, 'https://$10.ssl.qhimg.com');
+    }
+    element.src = src;
+    var node=document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(element, node);
+})();
+
